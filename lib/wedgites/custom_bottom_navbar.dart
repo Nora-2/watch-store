d4c6972b6f66e962f67bd6dff986be67c38a7bc6
mstfa_201 <mostafaa.sh2001@gmail.com>
2023-09-01 18:12:44 +0300

    create bottmNavBar

---
+++ lib/wedgites/custom_bottom_navbar.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+class CustomBottomNavBar extends StatelessWidget {
+  const CustomBottomNavBar({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(40),
+          boxShadow: const [
+            BoxShadow(
+              color: Colors.grey,
+              spreadRadius: 1,
+              blurRadius: 10,
+              offset: Offset(0, 5),
+            ),
+          ],
+        ),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(40),
+          child: BottomNavigationBar(
+            currentIndex: 0,
+            showUnselectedLabels: true,
+            selectedItemColor: Colors.black,
+            unselectedItemColor: Colors.grey,
+            unselectedLabelStyle: const TextStyle(color: Colors.grey, fontSize: 10),
+            selectedLabelStyle: const TextStyle(color: Colors.black, fontSize: 12),
+            items: [
+              BottomNavigationBarItem(
+                icon: Image.asset('assets/images/home_icon.png'),
+                label: 'Home',
+              ),
+              BottomNavigationBarItem(
+                icon: Image.asset('assets/images/products_icon.png'),
+                label: 'Products',
+              ),
+              BottomNavigationBarItem(
+                icon: Image.asset('assets/images/deals_icon.png'),
+                label: 'Deals',
+              ),
+              BottomNavigationBarItem(
+                icon: Image.asset('assets/images/profile_icon.png'),
+                label: 'Profile',
+              ),
+              BottomNavigationBarItem(
+                icon: Image.asset('assets/images/cart_icon.png'),
+                label: 'Cart',
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
