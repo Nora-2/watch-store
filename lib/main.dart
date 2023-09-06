ab30ea4dc76fdfba4e451cdcb6ce03401b5fd9e7
mstfa_201 <mostafaa.sh2001@gmail.com>
2023-09-06 15:18:20 +0300

    fix bug

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,8 @@
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
 import 'package:signin_signup/homelayout.dart';
-
-
-
 import 'cubit_home/homecubit_cubit.dart';
-
 import 'package:flutter_bloc/flutter_bloc.dart';
-=======
-import 'package:signin_signup/screens/splash/splash_screen.dart';
-import 'onBoarding/on_boarding.dart';
->>>>>>> aa2ccbcf1bb9a117ea39cbe7b54707ff418ca7a9
+
 
 void main() {
   runApp(const MyApp());
@@ -22,17 +14,13 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-<<<<<<< HEAD
     return MultiBlocProvider(
         providers: [
           BlocProvider(create: (context) => HomecubitCubit()),
         ],
       
     child:MaterialApp(
-=======
-    return MaterialApp(
->>>>>>> aa2ccbcf1bb9a117ea39cbe7b54707ff418ca7a9
-    
+
        title: 'Watch Store',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
@@ -41,13 +29,8 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
         fontFamily: 'Poppins',
       ),
-<<<<<<< HEAD
       home: const HomeLayout(),
     ));
-=======
-      home: const SplashScreen(),
-    );
->>>>>>> aa2ccbcf1bb9a117ea39cbe7b54707ff418ca7a9
   }
 }
 
