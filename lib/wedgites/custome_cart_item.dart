e697faf84613ae27efc4baaeb6f069442dc12944
Eman Mahfouz <emankahled614@gmail.com>
2023-09-08 13:58:57 +0300

    cart

---
+++ lib/wedgites/custome_cart_item.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:signin_signup/model/watch_Item_model.dart';
+
+class CutomCartItem extends StatelessWidget {
+  const CutomCartItem({super.key, required this.item,
+  });
+
+  final WatchItem item;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Container(
+        padding: EdgeInsets.only(right: 10),
+        height: 130,
+        width: double.infinity,
+        decoration: BoxDecoration(
+          color: Color(0xffFFFFFF),
+          borderRadius: BorderRadius.circular(9),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.3),
+              blurRadius: 4,
+              offset: Offset(2, 2),
+            ),
+          ],
+        ),
+        child: Row(
+          children: [
+            Image.asset(
+              item.image,
+              width: 90,
+              height: 90,
+            ),
+            Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    item.watchName,
+                    softWrap: false,
+                    overflow: TextOverflow.ellipsis,
+                    style: const TextStyle(
+                        fontSize: 13, fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    item.watchDescription,
+                    style: const TextStyle(
+                        fontSize: 7, fontWeight: FontWeight.bold),
+                  ),
+                  const Row(
+                    children: [
+                      Icon(Icons.arrow_back_ios),
+                      Text(
+                        'Show Product',
+                        style: TextStyle(
+                            fontSize: 15, fontWeight: FontWeight.bold),
+                      )
+                    ],
+                  )
+                ]),
+            const Spacer(
+              flex: 2,
+            ),
+            Text(
+              item.price,
+              style: const TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
